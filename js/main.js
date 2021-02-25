2864496e0de30e664f624d762fed8cd07bfbc7e2
Laura Caldaronello <caldaronello.l@gmail.com>
2021-02-25 13:36:14 +0100

    Ok ma si può ottimizzare

--- js/main.js
+++ js/main.js
@@ -1,10 +1,13 @@
 var numeri = [];
 for (var i = 0; i < 100; i++) {
-  if ((i + 1) % 3 == 0) {
+  if ((i + 1) % 3 == 0 && (i + 1) % 5 != 0) {
     numeri[i] = 'Fizz';
   }
-  else if ((i + 1) % 5 == 0) {
+  else if ((i + 1) % 5 == 0 && (i + 1) % 3 != 0) {
     numeri[i] = 'Buzz';
+  }
+  else if ((i + 1) % 3 == 0 && (i + 1) % 5 == 0) {
+    numeri[i] = 'FizzBuzz';
   }
   else {
     numeri[i] = i + 1;
