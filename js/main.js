5f42e3062f07a6078c5840726de75c3268ffad98
Laura Caldaronello <caldaronello.l@gmail.com>
2021-02-25 13:33:18 +0100

    Quasi

--- js/main.js
+++ js/main.js
@@ -1,5 +1,13 @@
 var numeri = [];
 for (var i = 0; i < 100; i++) {
-  numeri[i] = i + 1;
+  if ((i + 1) % 3 == 0) {
+    numeri[i] = 'Fizz';
+  }
+  else if ((i + 1) % 5 == 0) {
+    numeri[i] = 'Buzz';
+  }
+  else {
+    numeri[i] = i + 1;
+  }
 }
 console.log(numeri);
